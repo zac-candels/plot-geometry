8e8dac549667b123a0245927d647d37d0c4c437d
zcandels <s2767572@ed.ac.uk>
2025-02-05 17:18:48 +0000

    Changed quite a bit. Refactored the program so that it's now in standard form. Defined a class BoundaryNode for the boundary nodes and collecting all instances of the BoundaryNode class in a dictionary, with the node coordinates as the key and the class data as the value.

--- plot_geometry.py
+++ plot_geometry.py
@@ -5,7 +5,22 @@ from scipy.optimize import fsolve
 
 plt.close('all')
 
-class BdyPts:
+"""
+class GridNode:
+    def __init__(self, x, y, node_type):
+        self.x = x
+        self.y = y
+        self.type = node_type
+
+        
+class BoundaryNode(GridNode):
+    def __init__(self, x, y):
+        super().__init__(x, y, "boundary")
+    distances = [] #np.array(distances)
+    normals = [] #np.array(normals)
+"""
+
+class BoundaryNode:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -13,43 +28,21 @@ class BdyPts:
     normals = []
 
 
-alpha = 0.2
-R = 1
+def make_grid(x_min, x_max, y_min, y_max, n_pts):
+    
+    x_pts = np.linspace(x_min, x_max, n_pts)
+    y_pts = np.linspace(y_min,y_max,n_pts)
+    grid_pts = np.zeros([n_pts**2, 3])
+    for i in range(len(x_pts)):
+        for j in range(len(x_pts)):
+            grid_pts[i + j*n_pts, 0] =  x_pts[i]
+            grid_pts[i + j*n_pts, 1] = y_pts[j]
+    
+    return grid_pts
 
-
-x_vals = np.linspace(-1,1.1,100000)
-n_pts = 10
-x_min, x_max = -1, 1
-y_min, y_max = 0, 2
-x_pts = np.linspace(-1, 1.1, n_pts)
-dx = x_pts[1] - x_pts[0]
-y_pts = np.linspace(0,2,n_pts)
-dy = y_pts[1] - y_pts[0]
-grid_pts = np.zeros([n_pts**2, 3])
-
-vels = np.array([
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0,-1],
-    [np.sqrt(2)/2, np.sqrt(2)/2],
-    [-np.sqrt(2)/2, np.sqrt(2)/2],
-    [np.sqrt(2)/2, -np.sqrt(2)/2],
-    [-np.sqrt(2)/2, -np.sqrt(2)/2]   ])
-
-
-for i in range(len(x_pts)):
-    for j in range(len(x_pts)):
-        grid_pts[i + j*n_pts, 0] =  x_pts[i]
-        grid_pts[i + j*n_pts, 1] = y_pts[j]
-
-X, Y = np.meshgrid(x_pts, y_pts)
-
-def f(t):
-    return 0. + t*0 - np.sqrt(R**2 - (0.643 + t*1 + alpha)**2)
     
 
-def bdy_curve_left(x, y, N):
+def bdy_curve_left(x, y, N, alpha, R):
     if -N - alpha*1/2 < x < R -N - alpha:
         return y - np.sqrt(R**2 - (x + alpha + N)**2)
     else:
@@ -59,7 +52,7 @@ def bdy_curve_left_directional(t, x0, y0, v, N):
     v_x, v_y = v
     return bdy_curve_left(x0 + t*v_x, y0 + t*v_y, N)
 
-def bdy_curve_right(x, y, N):
+def bdy_curve_right(x, y, N, alpha, R):
     if -N - alpha*1/2 < x < R - N:
         return y - np.sqrt(R**2 - (x + N)**2)
     else:
@@ -70,91 +63,148 @@ def bdy_curve_right_directional(t, x0, y0, v, N):
     return bdy_curve_right(x0 + t*v_x, y0 + t*v_y, N)
 
 # Compute y-values
-def fn_left(x, N):
+def fn_left(x, N, alpha, R):
     if -N - alpha*1/2 < x < R -N - alpha:
         return np.sqrt(R**2 - (x + alpha + N)**2)
     else:
         return 
 
-def fn_right(x, N):
+def fn_right(x, N, alpha, R):
     if -N - alpha*1/2 < x < R - N:
         return np.sqrt(R**2 - (x + N)**2)
     else:
         return
 
 
-N = 1
 
-# Create boundary curves
-y_l = np.zeros([len(x_vals), 5])
-y_r = np.zeros([len(x_vals), 5])
-for i in range(N):
-    for j in range(len(x_vals)):
-        y_l[j, i] = fn_left(x_vals[j], i)
-        y_r[j, i] = fn_right(x_vals[j], i)
-    
-    
-    
-    
-# Label solid nodes
-fluid_pts = []
-solid_pts = []
-bdy_pts = []
-for k in range(n_pts**2):
-    if grid_pts[k,0] < np.sqrt(R**2 - grid_pts[k,1]**2):
-        if grid_pts[k,0] > np.sqrt(R**2\
-                                     - grid_pts[k,1]**2\
-                                         - 2*grid_pts[k,0]*alpha - alpha**2):
-            grid_pts[k,2] = 2
-            solid_pts.append( [ grid_pts[k,0], grid_pts[k,1] ] )
 
-solid_pts = np.asarray(solid_pts)
+
+def label_solid_nodes(grid_pts, R, alpha):
+    n_pts_tot = len(grid_pts[:,0])
+    solid_pts = []
+    for k in range(n_pts_tot):
+        if R**2 - grid_pts[k,1]**2 < 0:
+            continue
+        if R**2 - grid_pts[k,1]**2 - 2*grid_pts[k,0]*alpha - alpha**2 < 0:
+            continue
+        
+        if grid_pts[k,0] < np.sqrt(R**2 - grid_pts[k,1]**2):
+            if grid_pts[k,0] > np.sqrt(R**2\
+                                         - grid_pts[k,1]**2\
+                                             - 2*grid_pts[k,0]*alpha - alpha**2):
+                grid_pts[k,2] = 2
+                solid_pts.append( [ grid_pts[k,0], grid_pts[k,1] ] )
+    
+    solid_pts = np.asarray(solid_pts)
+    return solid_pts
             
                 
 # Label boundary nodes
-for i in range(len(grid_pts)):
-    if grid_pts[i,2] == 2:
-        continue
-    else:
-        p = np.array([ grid_pts[i,0], grid_pts[i,1] ])
-        for j in range(len(solid_pts)):
-            q = np.array([ solid_pts[j,0], solid_pts[j,1] ])
-            if np.linalg.norm(p - q) < np.sqrt(2)*np.max([dx,dy]):
-                grid_pts[i,2] = 1
-                bdy_pts.append( [ grid_pts[i,0], grid_pts[i,1] ] )
-                
-bdy_pts = np.asarray(bdy_pts)
+def label_bdy_nodes(grid_pts, solid_pts, R, alpha):
+    x_pts = grid_pts[:,0]
+    dx = x_pts[1] - x_pts[0]
+    
+    y_pts = np.unique(grid_pts[:,1])
+    dy = y_pts[1] - y_pts[0]
+    bdy_pts = []
+    boundary_nodes = {}
+    for i in range(len(grid_pts)):
+        if grid_pts[i,2] == 2:
+            continue
+        else:
+            p = np.array([ grid_pts[i,0], grid_pts[i,1] ])
+            for j in range(len(solid_pts)):
+                q = np.array([ solid_pts[j,0], solid_pts[j,1] ])
+                if np.linalg.norm(p - q) < np.sqrt(2)*np.max([dx,dy]):
+                    grid_pts[i,2] = 1
+                    x0, y0 = grid_pts[i,0], grid_pts[i,1]
+                    bdy_pts.append( [ x0, y0 ] )
+                    boundary_nodes[(x0, y0)]
+                    
+    bdy_pts = np.asarray(bdy_pts)
+    return boundary_nodes
 
 
-distances = []
-t_vals = np.linspace(0,2,100) 
-misc_vals = np.zeros(100)
 # Loop through all boundary points, calculate
 # distances to the wall and normal vectors to the bdy curves.
-for i in range(len(bdy_pts)):
-    for j in range(len(vels)):
-        x0, y0 = bdy_pts[i, 0], bdy_pts[i, 1]
-        if bdy_curve_left(x0, y0, N) == np.nan or bdy_curve_right(x0, y0, N) == np.nan:
-            continue
-        vel_vec = vels[j, :]
-        #f = lambda t: bdy_curve_right_directional(t, x0, y0, vel_vec, N)
-        
+def distances_and_normals(boundary_nodes, R, alpha, vels, N):
+    distances = []
+    for (x0,y0), _ in boundary_nodes.items:
+        for j in range(len(vels)):
+            v_x, v_y = vels[j, :]
+            coeffs = [v_x**2 + v_y**2, 2*(y0*v_x + x0*v_y + v_x*alpha),\
+                      y0**2 - R**2 + x0**2 + alpha**2 + 2*x0*alpha]
+            roots = np.roots(coeffs)
+            if np.iscomplexobj(roots) == True:
+                continue
+            
+            if len(roots) == 1 and roots > 0:
+                distances.append(roots)
+            elif len(roots) == 1 and roots < 0:
+                continue
+            elif len(roots) == 2 and np.all(roots > 0):
+                distances.append(np.min(roots))
+            elif len(roots) == 2 and np.all(roots < 0):
+                continue
+            elif len(roots) == 2 and np.any(roots < 0):
+                distances.append( roots[roots> 0])
+            
+            
+            #f = lambda t: bdy_curve_right_directional(t, x0, y0, vel_vec, N)
+            
+            
 
-        sol = scipy.optimize.root_scalar(f, bracket=[0, R-alpha], method='brentq')
-        
+
+def visualize(x_vals, y_l, y_r, N, alpha, R):
+    
+    y_l = np.zeros([len(x_vals), 5])
+    y_r = np.zeros([len(x_vals), 5])
+    for i in range(N):
+        for j in range(len(x_vals)):
+            y_l[j, i] = fn_left(x_vals[j], i)
+            y_r[j, i] = fn_right(x_vals[j], i)
+            
+    plt.figure()
+    for i in range(N):
+        plt.plot(x_vals, y_l[:,i])
+        plt.plot(x_vals, y_r[:,i] )
+        plt.xlim([-(N-1)-alpha*1/2-0.1, R - (N-1) + 0.1])
+        plt.axhline(0, color='black')
+        #plt.scatter(X, Y, color="black", s=1)
+        #plt.scatter(solid_pts[:,0],solid_pts[:,1], color="red", marker="o", s=1)
+        #plt.scatter(bdy_pts[:,0],bdy_pts[:,1], color="green", marker="o", s=5)
 
 
 
-
-plt.figure()
-for i in range(N):
-    plt.plot(x_vals, y_l[:,i])
-    plt.plot(x_vals, y_r[:,i] )
-    plt.xlim([-(N-1)-alpha*1/2-0.1, R - (N-1) + 0.1])
-    plt.axhline(0, color='black')
-    #plt.scatter(X, Y, color="black", s=1)
-    #plt.scatter(solid_pts[:,0],solid_pts[:,1], color="red", marker="o", s=1)
-    #plt.scatter(bdy_pts[:,0],bdy_pts[:,1], color="green", marker="o", s=5)
+def main():
+    vels = np.array([
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0,-1],
+        [np.sqrt(2)/2, np.sqrt(2)/2],
+        [-np.sqrt(2)/2, np.sqrt(2)/2],
+        [np.sqrt(2)/2, -np.sqrt(2)/2],
+        [-np.sqrt(2)/2, -np.sqrt(2)/2]   ])
+    
+    N = 1 # Number of times boundary shape is repeated
+    n_pts_per_direction = 10
+    x_min, x_max = -1, 1
+    y_min, y_max = 0, 2
+    R = 1
+    alpha = 0.8
+    x_vals = np.linspace(x_min,x_max + 0.1,100000)
+    
+    grid_pts = make_grid(x_min, x_max, y_min, y_max, n_pts_per_direction)
+    solid_pts = label_solid_nodes(grid_pts, R, alpha)
+    boundary_nodes = label_bdy_nodes(grid_pts, solid_pts, R, alpha)
+    
+    #distances_and_normals(boundary_nodes, bdy_curve, vels, N)
+    
+    #visualize(x_vals, N, alpha, R)
+    
+    
+main()
     
     
     
