50275cf3bf5f60555cf154bc7f8e164012cd63d5
zcandels <s2767572@ed.ac.uk>
2025-02-07 16:22:52 +0000

    Added a function 'label_bdy_points' which - as the name suggests - determines which nodes are boundary nodes.

--- plot_geometry2.py
+++ plot_geometry2.py
@@ -3,7 +3,27 @@ import matplotlib.pyplot as plt
 import scipy
 from shapely.geometry import Point, Polygon
 
+# This program does several things.
+# 1. Contains a function to define the geometry of the system 
+# and create points for the boundary curve(s)
+# 2. Function to label the solid points.
+# 3. Function to label boundary points
+# 4. Function to compute distances and normals from boundary points
+# to the boundary curve. 
+
+
+# Note, to determine the position of a grid point in the c++ code,
+# use the function ComputeXYZ in the service.hh header file.
+
 plt.close('all')
+
+class BoundaryNode:
+    def __init__(self, x, y, directions):
+        self.x = x
+        self.y = y
+        self.directions = directions
+    distances = []
+    normals = []
 
 
 def bdy_fn_left(x, N_repeats, alpha, R):
@@ -64,22 +84,67 @@ def label_solid_pts(grid_pts, left_bdy_pts, right_bdy_pts):
     # Label points inside the crescent moon
     solid_points = []
     pts_x, pts_y = [], []
-    for point in mesh_pts:
+    for idx, point in enumerate(mesh_pts):
         if bdy_shape_polygon.contains(Point(point)):
             pts_x.append(point[0])
             pts_y.append(point[1])
             
             solid_points.append(point)
+            grid_pts[idx,2] = 2
     solid_points = np.transpose( np.asarray( [ pts_x, pts_y ] ) )
     return solid_points
 
 
-def visualize(grid_pts, left_bdy_pts, right_bdy_pts, solid_pts):
+def label_bdy_points(grid_pts, solid_pts):
+    x_pts = grid_pts[:,0]
+    dx = x_pts[1] - x_pts[0]
+    
+    y_pts = np.unique(grid_pts[:,1])
+    dy = y_pts[1] - y_pts[0]
+    bdy_pts = []
+    boundary_nodes = {}
+    eps = 1e-5
+    for i in range(len(grid_pts)):
+        directions = []
+        if grid_pts[i,2] == 2:
+            continue
+        else:
+            p = np.array([ grid_pts[i,0], grid_pts[i,1] ])
+            for j in range(len(solid_pts)):
+                q = np.array([ solid_pts[j,0], solid_pts[j,1] ])
+                if np.linalg.norm(p - q) <= np.sqrt(2)*np.max([dx,dy]) + eps:
+                    directions.append( (p - q)/np.linalg.norm(p - q) )
+                    grid_pts[i,2] = 1
+                    x0, y0 = grid_pts[i,0], grid_pts[i,1]
+                    bdy_pts.append( [ x0, y0 ] )
+                    for k in range(len(solid_pts)):
+                        if k == j:
+                            continue
+                        q = np.array([ solid_pts[k,0], solid_pts[k,1] ])
+                        if np.linalg.norm(p - q)\
+                            <= np.sqrt(2)*np.max([dx,dy]) + eps:
+                                
+                           directions.append( (p - q)/np.linalg.norm(p - q) ) 
+                            
+                    bdy_pt = BoundaryNode(x0, y0, directions)
+                    boundary_nodes[(x0, y0)] = bdy_pt
+                    break
+                    
+    return boundary_nodes
+
+def distances_and_normals(bdy_pts, left_bdy_pts, right_bdy_pts):
+    pass
+
+
+def visualize(grid_pts, left_bdy_pts, right_bdy_pts, solid_pts, bdy_nodes):
+    bdy_pts = np.asarray(list(bdy_nodes.keys()))
+    
     plt.figure()
     plt.scatter(grid_pts[:,0], grid_pts[:,1])
     plt.plot(left_bdy_pts[:,0], left_bdy_pts[:,1])
     plt.plot(right_bdy_pts[:,0], right_bdy_pts[:,1])
     plt.scatter(solid_pts[:,0], solid_pts[:,1], color="red")
+    plt.scatter(bdy_pts[:,0], bdy_pts[:,1], color="green")
     
 
 def main():
@@ -94,12 +159,18 @@ def main():
     
     grid_pts = make_grid(x_min, x_max, y_min, y_max, n_grid_pts)
     
-    left_bdy_pts, right_bdy_pts = define_bdy_curve(alpha, R,
+    left_bdy_curve_pts, right_bdy_curve_pts = define_bdy_curve(alpha, R,
                                            N_bdy_pts, N_repeats, x_min, x_max)
     
-    solid_points = label_solid_pts(grid_pts, left_bdy_pts, right_bdy_pts)
+    solid_pts = label_solid_pts(grid_pts, left_bdy_curve_pts,
+                                right_bdy_curve_pts)
     
-    visualize(grid_pts, left_bdy_pts, right_bdy_pts, solid_points)
+    bdy_nodes = label_bdy_points(grid_pts, solid_pts)
+    
+    distances_and_normals(bdy_nodes, left_bdy_curve_pts, right_bdy_curve_pts)
+    
+    visualize(grid_pts, left_bdy_curve_pts,
+              right_bdy_curve_pts, solid_pts, bdy_nodes)
     
     return 
     
